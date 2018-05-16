25c4b5c31e57f37ca609709dd3555bc05286ba70
pooja reddy <38490165+pooja18reddy@users.noreply.github.com>
2018-05-16 08:43:47 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host="localhost";
-$db_user="root";
-$db_password="tiger";
-$db_name="2015cse099";
-$dbh=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die("Error connecting to the database");
-?>
